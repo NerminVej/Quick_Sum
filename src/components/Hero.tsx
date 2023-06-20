a10b713acc004c9a314353a3c2d5dc87444db931
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-20 10:11:44 +0200

    fixed a typo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { logo } from "../assets;";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-        
+      <nav className="flex justify-between itemms-center flex-col">
+        <img src={logo} alt="sumz_log" className="w-28 object-contain"></img>
+      </nav>
     </header>
   );
 };
