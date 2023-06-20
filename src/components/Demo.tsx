7287a193b8d7c4ac9a2a5417839d93bcd2809171
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-20 12:51:48 +0200

    added url handler

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -37,8 +37,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
